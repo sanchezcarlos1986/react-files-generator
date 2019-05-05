cd55dfcffc57e93ea2dffc097fc6131199518ea9
Carlos Sánchez <sanchez.carlos.1986@gmail.com>
2019-05-05 00:22:25 -0300

    fix(components): Hola component added

--- src/components/Dashboard/style.js
+++
@@ -1,8 +0,0 @@
-/**
- * Put your styles here. 
- * This document can receive props from the component, as it's working with Styled Components.
- */
-import styled from 'styled-components'
-
-const StyleWrapper = styled.div``
-export default StyleWrapper
